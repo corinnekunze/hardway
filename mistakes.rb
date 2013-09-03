c21c361c7903c9db92b0051735819df05f0f38b6
ckunze <corinne@planetargon.com>
2013-09-03 12:04:31 -0700

    first push

---
+++ mistakes.rb
@@ -0,0 +1,8 @@
+
+puts "ARGV = var1, var2, var3"
+puts "Remember to declare the variables next to ARGV then you can:\n
+Your first variable is: \#\{var1\}\n
+Your 2nd variable is: \#\{var2\}\n
+Your 3rd variable is: \#\{var3\}\n"
+puts "================"
+puts "We need STDIN in front of gets.chomp() to get input from user in the case of ARGV"
